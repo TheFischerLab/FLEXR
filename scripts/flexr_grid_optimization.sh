6dd8d0eae092bca7c985c767135ab90a177549c1
stachowskitim <tstachowski@hwi.buffalo.edu>
2023-01-18 11:47:55 -0600

    updated grid param script

--- scripts/flexr_grid_optimization.sh
+++ scripts/flexr_grid_optimization.sh
@@ -3,24 +3,27 @@
 ## an example of the FLEXR workflow with a grid optimization of parameters
 ## author: Tim Stachowski, PhD
 
+# define paths to coot1 and python3
+cootdir=/opt/homebrew/Cellar/coot/1.0.05/bin/coot
+python3dir=/opt/homebrew/bin/python3
+
 # make list of PDBs
 ls ????.pdb > pdb.list
 
-
+# loop over models in input list
 for i in `cat pdb.list`
 do
 
-i=${i%.*}
+i=${i%.pdb*}
 
 # remove previously built alt confs
 phenix.pdbtools ${i}".pdb" remove_alt_confs=True
-mv ${i}"_modified.pdb" ${i}".pdb"
 
 # create maps
-phenix.maps ${i}".pdb" ${i}".mtz"
+phenix.maps ${i}"_modified.pdb" ${i}".mtz"
 
 # run ringer
-mmtbx.ringer ${i}".pdb" ${i}"_map_coeffs.mtz"
+mmtbx.ringer ${i}"_modified.pdb" ${i}"_modified_map_coeffs.mtz"
 
 # test different sigma cutoffs
 for j in 0.30 0.35 0.40
@@ -31,16 +34,18 @@ for k in 30 35 40
 do
 
 # run flexr-find
-/opt/homebrew/bin/python3 flexr_find.py -f ${i}"_ringer.csv" -t ${j} -g ${k}
-mv "peak_finder_output_"${i}"_ringer.csv" "peak_finder_output_"${i}"_"${j}"_"${k}"_ringer.csv"
-cp $i".pdb" ${i}"_"${j}"_"${k}".pdb"
-cp $i".mtz" ${i}"_"${j}"_"${k}".mtz"
-mv ${i}"_ringer_alts.csv" ${i}"_"${j}"_"${k}"_ringer_alts.csv"
+${python3dir} flexr_find.py -f ${i}"_modified_ringer.csv" -t ${j} -g ${k}
 
 # run flexr-build
-/opt/homebrew/Cellar/coot/1.0.05/bin/coot --script flexr_build.py file-${i}"_"${j}"_"${k} #load_maps-True score-True
-rm ${i}"_"${j}"_"${k}".pdb"
+${cootdir} --script flexr_build.py file.${i}"_modified"
 
+# rename output files with grid parameters
+# output of peak finding
+mv "peak_finder_output_"${i}"_modified_ringer.csv" "peak_finder_output_"${i}"_"${j}"_"${k}"_modified_ringer.csv"
+# identified alt confs
+mv ${i}"_modified_ringer_alts.csv" ${i}"_"${j}"_"${k}"_modified_ringer_alts.csv"
+# FLEXR built model
+mv ${i}"_modified_newconfs.pdb" ${i}"_"${j}"_"${k}"_modified_newconfs.pdb"
 
 done
 done
